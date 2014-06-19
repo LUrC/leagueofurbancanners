f7d6dcc0e6bc894792970adb95cdfb3c6d22948b
Rebecca Nesson <rebecca@prx.org>
2014-06-18 21:58:34 -0400

    removing carrierwave initializer from repo

--- config/initializers/carrierwave.rb
+++
@@ -1,8 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAJ6UVUMX6VW2RVO4Q',                        # required
-    :aws_secret_access_key  => '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'   # required
-  }
-  config.fog_directory  = 'lurc-trees'                     # required
-end
